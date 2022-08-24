180a3174554f59f6fe63bab06f51b506aeb49daa
Tim <0xtimc@gmail.com>
2022-08-24 17:43:46 +0100

    Add dark mode button

--- src/js/toggleDarkMode.js
+++ src/js/toggleDarkMode.js
@@ -1,23 +1,15 @@
-const toggleSwitch = document.querySelector('#theme-switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector('#theme-switch');
 
 //function that changes the theme, and sets a localStorage variable to track the theme between page loads
 function switchTheme(e) {
-    if (e.target.checked) {
+    if (document.documentElement.classList.contains("dark")) {
+        localStorage.setItem('theme', 'light');
+        document.documentElement.classList.remove("dark");
+    } else {
         localStorage.setItem('theme', 'dark');
         document.documentElement.classList.add("dark");
-        toggleSwitch.checked = true;
-    } else {
-        localStorage.setItem('theme', 'light');
-        // document.documentElement.setAttribute('data-theme', 'light');
-        document.documentElement.classList.remove("dark");
-        toggleSwitch.checked = false;
     }    
 }
 
 // listener for changing themes
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-// Pre-check the dark-theme checkbox if dark-theme is set
-if (document.documentElement.classList.contains("dark")) {
-    toggleSwitch.checked = true;
-}
+toggleSwitch.addEventListener('click', switchTheme, false);
