b6e87718c3235d2b4ce90ab073cd6ff9bb5c49c7
Tim <0xtimc@gmail.com>
2022-12-29 09:27:19 +0000

    Just get JS stuff working for now

--- esbuild.mjs
+++ esbuild.mjs
@@ -5,13 +5,13 @@ import { sassPlugin } from 'esbuild-sass-plugin'
 
 try {
   await esbuild.build({
-    entryPoints: ['src/js/main.js', 'src/scss/main.scss'],
+    entryPoints: ['src/js/main.js'/*, 'src/scss/main.scss'*/],
     outdir: 'Example',
     bundle: true,
     sourcemap: true,
     minify: true,
     watch: process.argv.includes('--watch'),
-    plugins: [sassPlugin()],
+    // plugins: [sassPlugin()],
     external: ['/src/fonts/*', '/src/images/*', '/src/index.html'],
   })
 } catch {
