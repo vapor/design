45654fc329e467154a2da45641e4e5fa91b6aecd
Tim <0xtimc@gmail.com>
2022-12-30 22:25:13 +0000

    Get it compiling

--- Sources/VaporDesign/Components/SiteHeader.swift
+++ Sources/VaporDesign/Components/SiteHeader.swift
@@ -5,9 +5,9 @@ public struct SiteHeader<Site: Website>: Component {
     var context: PublishingContext<Site>
     var selectedSelectionID: Site.SectionID?
     
-    public init(context: PublishingContext<Site>, selectedSectionID: Site.SectionID?) {
+    public init(context: PublishingContext<Site>, selectedSelectionID: Site.SectionID?) {
         self.context = context
-        self.selectedSelectionID = selectedSectionID
+        self.selectedSelectionID = selectedSelectionID
     }
 
     public var body: Component {
@@ -18,23 +18,6 @@ public struct SiteHeader<Site: Website>: Component {
                     .img(.alt("Vapor Logo"), .src("/static/images/header-logo.png"), .class("d-iblock va-baseline")),
                     .h1("The Vapor Blog", .class("d-iblock ml-4 va-text-bottom"))
                 ).class("site-name d-iblock")
-
-                if Site.SectionID.allCases.count > 1 {
-                    navigation
-                }
-            }
-        }
-    }
-
-    private var navigation: Component {
-        Navigation {
-            List(Site.SectionID.allCases) { sectionID in
-                let section = context.sections[sectionID]
-
-                return Link(section.title,
-                    url: section.path.absoluteString
-                )
-                .class(sectionID == selectedSelectionID ? "selected" : "")
             }
         }
     }
--- Sources/VaporDesign/Components/Wrapper.swift
+++ Sources/VaporDesign/Components/Wrapper.swift
@@ -1,13 +1,9 @@
 import Plot
 
-public struct Wrapper: ComponentContainer {
+struct Wrapper: ComponentContainer {
     @ComponentBuilder var content: ContentProvider
     
-    public init(content: @escaping ContentProvider) {
-        self.content = content
-    }
-    
-    public var body: Component {
+    var body: Component {
         Div(content: content).class("wrapper")
     }
 }
