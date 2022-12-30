c4abc8b78a73648687972de8c72e68ee672c2de4
Tim <0xtimc@gmail.com>
2022-12-30 14:51:39 +0000

    Get line numbers working in hightlightjs

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,5 @@ import '../scss/main.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-import './highlight.min.js'
-
-// import './highlightjs-line-numbers.min.js'
 import './startSyntaxHighlighting.js'
 import './toggleDarkMode.js'
--- src/js/startSyntaxHighlighting.js
+++ src/js/startSyntaxHighlighting.js
@@ -2,5 +2,9 @@ import './highlight.min.js'
 
 const hljs = require('./highlight.min.js');
 
+window.hljs = hljs;
+
+require('./highlightjs-line-numbers.min.js');
+
 hljs.highlightAll();
-// hljs.initLineNumbersOnLoad();
+hljs.initLineNumbersOnLoad();
