5090cb4046661232dfc1b8b81eaba8c34979eb5e
Tim <0xtimc@gmail.com>
2022-12-31 01:16:48 +0000

    Build out component section

--- Sources/DesignSite/DesignTheme.swift
+++ Sources/DesignSite/DesignTheme.swift
@@ -18,6 +18,8 @@ private struct VaporThemeHTMLFactory: HTMLFactory {
             SiteNavigation(context: context, selectedSelectionID: nil)
             H1("Hello")
             SiteFooter()
+            
+            
         }
         let builder = VaporDesign<Site>(siteLanguage: context.site.language, isLocal: true)
         return builder.buildHTML(for: index, context: context, body: body)
@@ -51,5 +53,63 @@ private struct VaporThemeHTMLFactory: HTMLFactory {
                             context: PublishingContext<Site>) throws -> HTML? {
         let builder = VaporDesign<Site>(siteLanguage: context.site.language, isLocal: true)
         return builder.buildHTML(for: page, context: context, body: .body())
+    }
+    
+    func buildExampleBlogPage() -> Component {
+        Div {
+            
+        }
+    }
+    
+    func buildComponentDemo() -> Component {
+        Div {
+            H1("Everything below this is for just showing components")
+
+            #warning("Extract to component")
+            Navigation {
+                List {
+                    ListItem {
+                        Link(url: "#") {
+                            Span() {
+                                Text("«")
+                            }.attribute(named: "aria-hidden", value: "true")
+                            Text("Previous")
+                        }.class("page-link page-link-next-previous").accessibilityLabel("Previous")
+                    }.class("page-item")
+                    ListItem {
+                        Span {
+                            Text("1")
+                        }.class("page-link")
+                    }.class("page-item active").attribute(named: "aria-current", value: "page")
+
+                    ListItem {
+                        Link("2", url: "#").class("page-link")
+                    }.class("page-item")
+                    ListItem {
+                        Link("3", url: "#").class("page-link")
+                    }.class("page-item")
+                    ListItem {
+                        Text("...")
+                    }.class("page-item")
+                    ListItem {
+                        Link("8", url: "#").class("page-link")
+                    }.class("page-item")
+                    ListItem {
+                        Link("9", url: "#").class("page-link")
+                    }.class("page-item")
+                    ListItem {
+                        Link("10", url: "#").class("page-link")
+                    }.class("page-item")
+                    ListItem {
+                        Link(url: "#") {
+                            Span() {
+                                Text("»")
+                            }.attribute(named: "aria-hidden", value: "true")
+                            Text("Next")
+                        }.class("page-link page-link-next-previous").accessibilityLabel("Next")
+                    }.class("page-item")
+                }.class("pagination justify-content-center")
+            }.accessibilityLabel("blog-pagination")
+        }.class("container")
     }
 }
