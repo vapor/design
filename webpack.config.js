ce6f9ef69b5d1d79b15d4cffaa109605cd460dd4
Paul Toffoloni <paultoffoloni@gmail.com>
2024-04-04 10:25:14 +0200

    Update CI

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'Output'),
+    publicPath: 'auto',
     library: {
       name: 'Vapor',
       type: 'var',
