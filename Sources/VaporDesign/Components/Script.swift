bcaecbb70dda73318627b2612403e64916564b88
Paul Toffoloni <paultoffoloni@gmail.com>
2024-06-23 15:31:13 +0200

    Make Script public once again

--- Sources/VaporDesign/Components/Script.swift
+++ Sources/VaporDesign/Components/Script.swift
@@ -3,12 +3,12 @@ import Plot
 /// Component used to render an `<script>` element for embetdding scripts.
 public struct Script: Component {
     /// The URL of the script.
-    var url: URLRepresentable
+    public var url: URLRepresentable
 
     /// Create a new script instance.
     /// - parameters:
     ///   - url: The URL of the script to include.
-    init(url: URLRepresentable) {
+    public init(url: URLRepresentable) {
         self.url = url
     }
 
