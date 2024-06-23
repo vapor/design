2783dd164a580bbe2ff783414c0dbfc01cbf38fd
Paul Toffoloni <69189821+ptoffy@users.noreply.github.com>
2024-06-23 15:27:54 +0200

    Make `Script` component public (#195)
    * Make Script component public
    
    * Update Script

--- Sources/VaporDesign/Components/Script.swift
+++ Sources/VaporDesign/Components/Script.swift
@@ -1,7 +1,7 @@
 import Plot
 
 /// Component used to render an `<script>` element for embetdding scripts.
-struct Script: Component {
+public struct Script: Component {
     /// The URL of the script.
     var url: URLRepresentable
 
@@ -11,8 +11,8 @@ struct Script: Component {
     init(url: URLRepresentable) {
         self.url = url
     }
-    
-    var body: Component {
+
+    public var body: Component {
         Node<HTML.BodyContext>.script(.src(url))
     }
 }
