747a4d176c886140df7bb23f0e2c7b4bf689a4b0
Paul Toffoloni <paultoffoloni@gmail.com>
2024-07-11 12:10:59 +0200

    Add an actual formatter

--- static/js/mainSiteUpdateStarsCount.js
+++ static/js/mainSiteUpdateStarsCount.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('https://api.github.com/repos/vapor/vapor')
         .then(response => response.json())
         .then(data => {
-            const starCount = data.stargazers_count.toLocaleString('en-US');
-            document.getElementById('main-page-callout-stars-count').textContent = `${starCount} stars on GitHub`;
+            const starCount = data.stargazers_count;
+            const formattedStarCount = new Intl.NumberFormat('en-US').format(starCount);
+            document.getElementById('main-page-callout-stars-count').textContent = `${formattedStarCount} stars on GitHub`;
         })
         .catch(error => {
             console.error('Error fetching star count:', error);
