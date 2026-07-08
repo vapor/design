63f8a2e8940b5faa5d9bea1b27b1bb6be0fbe61d
Tim <0xtimc@gmail.com>
2026-07-08 21:19:10 +0100

    Fix the line number CSS

--- src/js/startSyntaxHighlighting.ts
+++ src/js/startSyntaxHighlighting.ts
@@ -1,3 +1,5 @@
+// Hacks the line number CSS because that's apparently not a thing you can turn off
+import './suppressLineNumberStyles';
 import hljs from './hljsSetup';
 import 'highlightjs-line-numbers.js';
 import CopyButtonPlugin from 'highlightjs-copy';
